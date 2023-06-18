8c93d11ce2c99f1bf733fdc3a7dda9a64b26c837
Joy <mmehedihasanjoyv@gmail.com>
2023-06-18 07:11:32 +0600

    created the error handling part

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // 1.Date Object:
 function getTodaysName(dateString) {
-    let dateParts = dateString.split("-");
-    let year = parseInt(dateParts[0]);
-    let month = parseInt(dateParts[1]) - 1;
-    let day = parseInt(dateParts[2]);
+    let YY_MM_DD = dateString.split("-");
+    let year = parseInt(YY_MM_DD[0]);
+    let month = parseInt(YY_MM_DD[1]) - 1;
+    let day = parseInt(YY_MM_DD[2]);
     let date = new Date(year, month, day);
     let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let dayOfWeek = daysOfWeek[date.getDay()];
@@ -12,9 +12,15 @@ function getTodaysName(dateString) {
 function getDay() {
     var inputElement = document.getElementById("datetime");
     var inputValue = inputElement.value;
-    console.log(inputValue);
-    let dayOfWeek =getTodaysName(inputValue);
-  document.getElementById("Date").innerText ="The day of the week: "+ dayOfWeek;
+    let dayOfWeek = getTodaysName(inputValue);
+    if (dayOfWeek === undefined) {
+        document.getElementById("Date").innerHTML = `
+        <h3 class="text-red-500 font-bold">Please Enter the valid date using This Formate(YY-MM-DD) </h3>
+        `
+    }
+    else {
+        document.getElementById("Date").innerText ="The day of the week: "+ dayOfWeek;
+    }
      return inputValue;
 }
 
