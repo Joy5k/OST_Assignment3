f34669532e5aa592e918f25fc3593c919a34e91d
Joy <mmehedihasanjoyv@gmail.com>
2023-06-19 06:47:09 +0600

    compelete the assignment

--- app.js
+++ app.js
@@ -120,13 +120,19 @@ function getCurrentLocation() {
     document.addEventListener('click', function(event) {
       const x = event.clientX;
       const y = event.clientY;
-      alert(`Clicked at coordinates (x, y): ${x}, ${y}`);
+
+        // Please comment out the alert message below.
+
+      // alert(`Clicked at coordinates (x, y): ${x}, ${y}`);
     });
 
     // b. Keydown event handler
     document.addEventListener('keydown', function(event) {
       const keyCode = event.keyCode || event.which;
-      alert(`Key pressed with key code: ${keyCode}`);
+
+      // Please comment out the alert message below.
+
+      // alert(`Key pressed with key code: ${keyCode}`);
     });
 
     // c. Mouseover event handler
@@ -147,4 +153,126 @@ document.addEventListener('DOMContentLoaded', function() {
       paragraph.textContent = 'Button Clicked!';
     });
   });
+
+// Form Input
+function validateForm(event) {
+  event.preventDefault();
+  const form = document.getElementById("myForm");
+  const name = form.elements.name;
+  const email = form.elements.email;
+  const password = form.elements.password;
+  const confirmPassword = form.elements.confirmPassword;
+
+  const nameError = document.getElementById("nameError");
+  const emailError = document.getElementById("emailError");
+  const passwordError = document.getElementById("passwordError");
+  const confirmPasswordError = document.getElementById("confirmPasswordError");
+
+  const namePattern = /^[a-zA-Z]+$/;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+  nameError.textContent = !namePattern.test(name.value) ? "Name should contain only letters." : "";
+  emailError.textContent = !emailPattern.test(email.value) ? "Please enter a valid email address." : "";
+  passwordError.textContent = !passwordPattern.test(password.value) ? "Password should have a minimum length of 8 characters, one uppercase letter, one lowercase letter, and one digit." : "";
+  confirmPasswordError.textContent = confirmPassword.value !== password.value ? "Confirm Password should match the Password field." : "";
+
+  if (!nameError.textContent && !emailError.textContent && !passwordError.textContent && !confirmPasswordError.textContent) {
+      alert("Form submitted successfully!");
+      form.reset();
+  }
+}
+// 10.DOM Manipulate CSS Class:
+function addHighlightClass() {
+  var paragraph = document.getElementById("Paragraph");
+  paragraph.classList.add("bg-yellow-400");
+}
+
+// 11.Create Element & Append Element:
+
+
+function createListItems() {
+  var ul = document.getElementById("myList");
+
+  for (var i = 1; i <= 5; i++) {
+      var li = document.createElement("li");
+      li.textContent =`This is the li number ${i}`;
+      li.classList.add("text-gray-800", "mb-2","bg-sky-400");
+      ul.appendChild(li);
+  }
+}
+function createLi() {
+  createListItems()
+}
+
+// 12.DOM Change Attribute Value:
+ function changeImage(){
+  let image = document.getElementById("changeImage");
+   image.src = "./images.jpg";
+   changeTextColor();
+}
+
+function changeTextColor() {
+  var paragraphs = document.querySelectorAll(".changeTextColor");
+
+  paragraphs.forEach(function(paragraph) {
+      paragraph.style.color = "red";
+  });
+}
+
+function getData() {
+  fetch("https://api.example.com/data")
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Request failed. Status code: " + response.status);
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      // Perform further actions with the response data
+    })
+    .catch(function (error) {
+      console.error("Request failed:", error);
+    });
+}
+
+// Call the function to send the GET request
+getData();
+
+
+
+function postData() {
+  var url = "https://api.example.com/submit";
+  var data = {
+    name: "John Doe",
+    email: "johndoe@example.com"
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Request failed. Status code: " + response.status);
+      }
+    })
+    .then(function (responseData) {
+      console.log(responseData);
+      // Perform further actions with the response data
+    })
+    .catch(function (error) {
+      console.error("Request failed:", error);
+    });
+}
+
+// Call the function to send the POST request
+postData();
 
